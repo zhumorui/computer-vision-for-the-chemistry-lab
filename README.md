a6de376b2f5c6a8e42889aaba24373acfa6bdf8c
morin <1130998349@qq.com>
2022-07-04 13:30:11 +0200

    add color change figure generation function

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This project is based on the vessel instance segmentation, which is implemented by [@sagieppel](https://github.com/sagieppel).
 
+```bash
+python main.py
+```
+
 Videos Resources link: https://drive.google.com/drive/folders/1xhWR0Rzq6qwpHGN3Rm18-Rby7yZWiOq-?usp=sharing
 
 Weight files link: https://drive.google.com/drive/folders/1SBjdqPskKwYyABW4xGnT_k6p7i7hskcn?usp=sharing
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2
 from Exp_v3 import Exp
 import time
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 cap = cv2.VideoCapture("Videos_Resources/Four Colour Change Reaction (Chameleon Chemical Reaction).mp4")
 #cap2 = cv2.VideoCapture("Videos_for_test/short_20210607_152134.mp4")
@@ -17,19 +20,24 @@ while True:
     if frame is not None:
 
         # time.sleep is only used in the demo for a better visualization.
-        time.sleep(0.04) 
+        # time.sleep(0.04) 
 
         image_with_mask,main_colors, color_distance = camera1.get_vessel_image_with_mask(frame)
+        color_distance_list = []
+        color_distance_list.append(color_distance)
+
         main_colors = str(main_colors)
 
         if color_distance >= 30 and color_distance <= 100:
             color_change_info = "detect color change! color distance = " + str(color_distance)
         elif color_distance > 100:
-            color_change_info = "detect abnormal color change!"
+            color_change_info = "detect abnormal color change!" + str(color_distance)
         else:
-            color_change_info = "no color change detect"
+            color_change_info = "no color change detect" + str(color_distance)
 
         font = cv2.FONT_HERSHEY_SIMPLEX
+
+        # main colors analysis text on frame
         cv2.putText(image_with_mask, 
                 main_colors, 
                 (50, 50), 
@@ -37,13 +45,16 @@ while True:
                 (0, 255, 255), 
                 2, 
                 cv2.LINE_4)
+
+        # color change information text on frame
         cv2.putText(image_with_mask, 
                 color_change_info, 
                 (50, 100), 
                 font, 0.6, 
                 (0, 255, 255), 
                 2, 
-                cv2.LINE_4)        
+                cv2.LINE_4)
+
         cv2.imshow('image_with_mask',image_with_mask)
     else:
         break
@@ -51,4 +62,15 @@ while True:
         break
 
 cap.release()
-cv2.destroyAllWindows()       
+cv2.destroyAllWindows() 
+
+# plot color change
+x = []
+for i in range(len(color_distance_list)):
+    x.append(i)
+
+
+
+plt.plot(x, color_distance_list)
+plt.savefig("color_change_figure.png")
+plt.show()
